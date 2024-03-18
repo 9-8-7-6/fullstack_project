5f21e5e342149533e4809db3d8146bf1da216a87
vito <ac1246897@gmail.com>
2024-03-18 12:15:46 +0800

    add usernav

--- frontend/project/app/components/navbar/Navbar.tsx
+++ frontend/project/app/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link'
 
 import SearchFilters from './SearchFilter';
+import UserNav from './UserNav';
 
 const Navbar = () => {
     return (
@@ -21,7 +22,8 @@ const Navbar = () => {
                     </div>
                     
                     <div className='flex items-center space-x-6'>
-                        Add property - User nav
+                        Add property
+                        <UserNav/>
                     </div>
                 </div>
             </div>
---
+++ frontend/project/app/components/navbar/UserNav.tsx
@@ -0,0 +1,22 @@
+
+const UserNav = () => {
+    return (
+        <div className="p-2 relative inline-block border rounded-full">
+            <button className="flex items-center">
+                
+            <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+            
+            <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+
+
+            </button>
+        </div>
+    )
+}
+
+export default UserNav
