2571187a2fcac0d2341a2ab157427ba4323c7814
vito <ac1246897@gmail.com>
2024-03-18 19:50:34 +0800

    add landlord detail

---
+++ frontend/project/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+    return(
+        <div className="mt-4 py-4 px-6 cursor-pointer bg-project text-white rounded-xl hover:bg-project-dark transition">
+            Contact
+        </div>
+    )
+}
+
+export default ContactButton;
---
+++ frontend/project/app/landlords/[id]/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+import ContactButton from "@/app/components/ContactButton";
+
+import PropertyList from "@/app/components/properties/PropertyList";
+
+const LandlordDetailPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            <div className="grid gird-col-1 md:grid-cols-4 gap-4">
+                <aside className="col-span-1 mb-4">
+                    <div className="flex flex-col items-center p-6 rounded-lg border border-gray-300 show-xl">
+                        <Image
+                            src='/buyer1.jpg'
+                            width={200}
+                            height={200}
+                            alt="Landlord name"
+                            className="rounded-full"
+                        />
+                        <h1 className="mt-6 text-2xl">Landlord Name</h1>
+                        
+                        <ContactButton />
+                    </div>
+                </aside>
+
+                <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
+                    <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-6">
+                        <PropertyList/>
+                            
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default LandlordDetailPage;
