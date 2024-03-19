5b5ed9d18ed46cbc40d37c11e66bb5fe3008a2dc
vito <ac1246897@gmail.com>
2024-03-19 14:21:57 +0800

    frontend set api

--- frontend/project/app/components/properties/PropertyListItem.tsx
+++ frontend/project/app/components/properties/PropertyListItem.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-const PropertyListItem = () => {
+import { PropertyType } from "./PropertyList";
+
+interface ProperProps {
+    property: PropertyType
+}
+
+const PropertyListItem: React.FC<ProperProps> = ({
+    property
+}) => {
     return(
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
-                    src='/101.jpg'
+                    src={property.image_url}
                     sizes="(max-width: 768px) 768px, (max-wideth: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="101大樓"
@@ -13,12 +21,12 @@ const PropertyListItem = () => {
             </div>
 
             <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+                <p className="text-lg font-bold">{property.title}</p>
 
             </div>
 
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>$200</strong> per night</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
         </div>
     )
